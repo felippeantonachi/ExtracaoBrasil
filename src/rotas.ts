a1721ca616835bc4fa8ed89a8778727f583e90ca
Clayton <claytonmenezes@hotmail.com>
2023-05-15 20:35:41 -0300

    alterações

--- src/rotas.ts
+++ src/rotas.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { conectar, desconectar, listar, buscar } from './db'
+import { conectar, desconectar, listar, buscar, filtrar } from './db'
 
 const defineRotas = () => {
   const router = Router()
@@ -24,8 +24,25 @@ const defineRotas = () => {
     try {
       const conexao = await conectar()
       try {
-        const numeroProcesso = req.query.numeroProcesso
-        const processos = await buscar(conexao, numeroProcesso as string)
+        const numeroProcesso = req.query.numeroProcesso as string
+        const processo = await buscar(conexao, numeroProcesso)
+        res.send(processo).status(200)
+      } catch (error) {
+        res.send(error).status(500)
+      } finally {
+        await  desconectar(conexao)
+      }
+    } catch (error) {
+      res.send(error).status(500)
+    }
+  })
+  router.get('/filtrar', async (req, res) => {
+    try {
+      const conexao = await conectar()
+      try {
+        const filtro = req.query.filtro
+        const processos = await filtrar(conexao, filtro as string)
+        console.log(processos.length)
         res.send(processos).status(200)
       } catch (error) {
         res.send(error).status(500)
