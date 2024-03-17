da879a00620114ab2b3187d5104ff84a61fc42f8
Clayton Menezes dos Santos <claytonmenezes@hotmail.com>
2024-03-17 02:57:39 -0300

    voltando o codigo do download

--- src/brasil.ts
+++ src/brasil.ts
@@ -5,15 +5,13 @@ import { conectar, desconectar, insereBulk } from './db'
 const iniciaProcessoArquivo = async () => {
   try {
     cron.schedule('0 3 * * *', async () => {
-      // await download()
-      // await extrair()
+      await download()
+      await extrair()
       let processos = await dbfToArray()
       const conexao = await conectar()
       try {
-        // processos = distinctByProperty(processos, 'PROCESSO')
+        processos = distinctByProperty(processos, 'PROCESSO')
         await insereBulk(processos)
-        // await insereProcessos(conexao, processos)
-        // await insereEvento(conexao, processos)
       } catch (error) {
         console.log(error)
       } finally {
